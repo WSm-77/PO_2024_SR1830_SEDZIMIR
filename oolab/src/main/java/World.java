5d2e8b0cda5e55f83f57c828fd5dd80d351445f5
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-10-11 12:20:14 +0200

    Add arguments parsing to run method

--- oolab/src/main/java/World.java
+++ oolab/src/main/java/World.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 
 public class World {
+    private final static String petName = "WSm";
+
     public static void main(String[] args) {
         // start
         System.out.println("system wystartował");
@@ -12,12 +14,25 @@ public class World {
     }
 
     public static void run(String[] args) {
-        System.out.println("WSm idzie do przodu!!!");
-
         // wyświetlenie listy argumentów
         var commaSeparatedArgs = String.join(", ", args);
         System.out.println("Lista argumentów:");
         System.out.println(commaSeparatedArgs);
+
+        // interpretacja argumentów
+        for (var arg : args) {
+            String directionMessage = switch (arg) {
+                case "f" -> "idzie do przodu";
+                case "b" -> "idzie do tyłu";
+                case "l" -> "skręca w lewo";
+                case "r" -> "skręca w prawo";
+                default -> "";
+                };
+
+            if (!directionMessage.isEmpty()) {
+                System.out.println(World.petName + " " + directionMessage);
+            }
+        }
 
     }
 }
