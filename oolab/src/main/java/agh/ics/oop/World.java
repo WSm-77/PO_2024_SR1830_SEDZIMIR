e84f155ec5ae9e75f9696ad6485697ecc6d19fb8
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-10-16 19:42:50 +0200

    Verify Vector2d class functionality

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,6 +1,7 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 public class World {
     private final static String petName = "WSm";
@@ -11,6 +12,12 @@ public class World {
 
         MoveDirection[] directions = OptionsParser.parseStringArray(args);
         World.run(directions);
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
 
         // stop
         System.out.println("system zakończył działanie");
