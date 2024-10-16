f13fe934759518828f7a8b9ea033bfc509911ead
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-10-16 19:38:50 +0200

    Add Vector2d class

---
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -0,0 +1,88 @@
+package agh.ics.oop.model;
+
+import java.util.Objects;
+
+public class Vector2d {
+    /*===========*\
+     * attributes *
+    \*===========*/
+    private final int x;
+
+    private final int y;
+
+    /*=========*\
+     * methods *
+    \*=========*/
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public String toString() {
+        return String.format("(%d,%d)", this.getX(), this.getY());
+    }
+
+    public boolean precedes(Vector2d other) {
+        return this.getX() <= other.getX() && this.getY() <= other.getY();
+    }
+
+    public boolean follows(Vector2d other) {
+        return this.getX() >= other.getX() && this.getY() >= other.getY();
+    }
+
+    public Vector2d add(Vector2d other) {
+        int newX = this.getX() + other.getX();
+        int newY = this.getY() + other.getY();
+        return new Vector2d(newX, newY);
+    }
+
+    public Vector2d subtract(Vector2d other) {
+        int newX = this.getX() - other.getX();
+        int newY = this.getY() - other.getY();
+        return new Vector2d(newX, newY);
+    }
+
+    public Vector2d upperRight(Vector2d other) {
+        int upperRightX = Math.max(this.getX(), other.getX());
+        int upperRightY = Math.max(this.getY(), other.getY());
+        return new Vector2d(upperRightX, upperRightY);
+    }
+
+    public Vector2d lowerLeft(Vector2d other) {
+        int upperRightX = Math.min(this.getX(), other.getX());
+        int upperRightY = Math.min(this.getY(), other.getY());
+        return new Vector2d(upperRightX, upperRightY);
+    }
+
+    public Vector2d opposite() {
+        return new Vector2d(-getX(), -getY());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        // check if object other is not null and both object are the same type
+        if (other == null || this.getClass() != other.getClass()) {
+            return false;
+        }
+
+        Vector2d otherVector2d = (Vector2d) other;
+        return x == otherVector2d.x && y == otherVector2d.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
