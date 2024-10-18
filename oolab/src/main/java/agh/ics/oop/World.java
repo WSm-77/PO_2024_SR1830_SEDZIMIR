f6a0d3295a1ea4e0c4290b839eaf61d6d57f6b20
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-10-18 10:42:28 +0200

    Add MapDirections' methods verification in main method

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,5 +1,6 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MapDirections;
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
 
@@ -13,11 +14,19 @@ public class World {
         MoveDirection[] directions = OptionsParser.parseStringArray(args);
         World.run(directions);
 
+        // Vector2d verification
         Vector2d position1 = new Vector2d(1,2);
         System.out.println(position1);
         Vector2d position2 = new Vector2d(-2,1);
         System.out.println(position2);
         System.out.println(position1.add(position2));
+
+        // MapDirections verification
+        MapDirections direction = MapDirections.NORTH;
+        System.out.println(direction.toString());
+        System.out.println(direction.next().toString());
+        System.out.println(direction.previous().toString());
+        System.out.println(direction.toUnitVector().toString());
 
         // stop
         System.out.println("system zakończył działanie");
