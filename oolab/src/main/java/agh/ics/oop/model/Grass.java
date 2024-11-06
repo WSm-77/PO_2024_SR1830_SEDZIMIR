47caf8f974581777f408bd625d5a16cbf81e1904
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-11-06 18:58:36 +0100

    Add and implement WorldElement interface

--- oolab/src/main/java/agh/ics/oop/model/Grass.java
+++ oolab/src/main/java/agh/ics/oop/model/Grass.java
@@ -1,14 +1,14 @@
 package agh.ics.oop.model;
 
-public class Grass {
+public class Grass implements WorldElement{
     public final static String GRASS_STRING = "*";
     private final Vector2d position;
-
 
     public Grass(Vector2d position) {
         this.position = position;
     }
 
+    @Override
     public Vector2d getPosition() {
         return this.position;
     }
