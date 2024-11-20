566db6df0802374366451b44444ac9f75cdc2b85
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-11-20 19:09:10 +0100

    Add exception throwing to OptionsParser.parseStringArray()

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -6,16 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OptionsParser {
+    public static final String ILLEGAL_MOVE_SUBMESSAGE = "is not legal move specification";
+
     public static List<MoveDirection> parseStringArray (String[] options) {
         ArrayList<MoveDirection> directions = new ArrayList<>();
 
         for (var option : options) {
             switch (option) {
-                case "f" -> directions.add(MoveDirection.FORWARD);
-                case "b" -> directions.add(MoveDirection.BACKWARD);
-                case "l" -> directions.add(MoveDirection.LEFT);
-                case "r" -> directions.add(MoveDirection.RIGHT);
-                default -> {}
+                case "f", "forward" -> directions.add(MoveDirection.FORWARD);
+                case "b", "backward" -> directions.add(MoveDirection.BACKWARD);
+                case "l", "left" -> directions.add(MoveDirection.LEFT);
+                case "r", "right" -> directions.add(MoveDirection.RIGHT);
+                default -> throw new IllegalArgumentException(String.format("%s %s", option, OptionsParser.ILLEGAL_MOVE_SUBMESSAGE));
             }
         }
 
