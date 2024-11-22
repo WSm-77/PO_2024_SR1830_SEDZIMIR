cc8b7df65610dfc4fb5167232239fd15915b3822
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-11-22 12:30:49 +0100

    Add changeCounter to ConsoleMapDisplay class

--- oolab/src/main/java/agh/ics/oop/model/util/ConsoleMapDisplay.java
+++ oolab/src/main/java/agh/ics/oop/model/util/ConsoleMapDisplay.java
@@ -4,9 +4,17 @@ import agh.ics.oop.model.MapChangeListener;
 import agh.ics.oop.model.WorldMap;
 
 public class ConsoleMapDisplay implements MapChangeListener {
+    public static final String MAP_COUNTER_CHANGE_MESSAGE_TEMPLATE = "%d. Change: %s";
+    private int changesCounter = 0;
+
+    private String createMapCounterChangeMessage(String message) {
+        return String.format(ConsoleMapDisplay.MAP_COUNTER_CHANGE_MESSAGE_TEMPLATE, this.changesCounter, message);
+    }
+
     @Override
     public void mapChanged(WorldMap worldMap, String message) {
-        System.out.println(message);
+        this.changesCounter++;
+        System.out.println(this.createMapCounterChangeMessage(message));
         System.out.println(worldMap);
     }
 }
