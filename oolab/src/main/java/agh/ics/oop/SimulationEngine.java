c356b91225d3928159a9dfcb7dc687c6f0751afd
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-11-29 10:54:48 +0100

    Add SimulationEngine.runAsyncInThreadPool() method

--- oolab/src/main/java/agh/ics/oop/SimulationEngine.java
+++ oolab/src/main/java/agh/ics/oop/SimulationEngine.java
@@ -2,8 +2,11 @@ package agh.ics.oop;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SimulationEngine {
+    public static final int NO_THREADS_IN_THREAD_POOL = 4;
     private final List<Simulation> simulationList;
     private final List<Thread> simulationThreadList = new ArrayList<>();
 
@@ -29,5 +32,15 @@ public class SimulationEngine {
         for (var simulationThread : this.simulationThreadList) {
             simulationThread.join();
         }
+    }
+
+    public void runAsyncInThreadPool() {
+        ExecutorService simulationsThreadPool = Executors.newFixedThreadPool(SimulationEngine.NO_THREADS_IN_THREAD_POOL);
+
+        for (var simulation : this.simulationList) {
+            simulationsThreadPool.submit(simulation);
+        }
+
+        simulationsThreadPool.shutdown();
     }
 }
