00379d3c73e686c82ae6b75b3077626baf19c2db
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-12-13 16:23:13 +0100

    Add GridPane and display map as grid

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -8,10 +8,15 @@ import agh.ics.oop.model.util.ConsoleMapDisplay;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.util.List;
 
@@ -22,6 +27,10 @@ public class SimulationPresenter implements MapChangeListener {
     public static final Vector2d ANIMAL1_STARTING_POSITION = new Vector2d(2, 2);
     public static final Vector2d ANIMAL2_STARTING_POSITION = new Vector2d(3, 4);
     public static final int TOTAL_GRASS_ON_GRASS_FIELD_MAP = 10;
+    public static final int CELL_WIDTH = 48;
+    public static final int CELL_HEIGHT = 48;
+
+    private WorldMap worldMap;
 
     @FXML
     private TextField movesTextEntry;
@@ -33,17 +42,60 @@ public class SimulationPresenter implements MapChangeListener {
     private Button startButton;
 
     @FXML
-    private Label infoLabel;
-
-    private WorldMap worldMap;
+    private GridPane mapGrid;
 
     public void setWorldMap(WorldMap map) {
         this.worldMap = map;
     }
 
+    private void buildMapGrid() {
+        var boundaries = this.worldMap.getCurrentBounds();
+
+        var upperLeft = new Vector2d(boundaries.lowerLeft().getX(), boundaries.upperRight().getY());
+
+        int gridRowCnt = boundaries.upperRight().getY() - boundaries.lowerLeft().getY();
+        int gridColumnCnt = boundaries.upperRight().getX() - boundaries.lowerLeft().getX();
+
+        this.setGridCellsSize(gridRowCnt, gridColumnCnt);
+
+        this.fillGridCells(gridRowCnt, gridColumnCnt, upperLeft);
+    }
+
+    private void setGridCellsSize(int rows, int columns) {
+        for (int i = 0; i < rows; i++){
+            mapGrid.getRowConstraints().add(new RowConstraints(CELL_HEIGHT));
+        }
+
+        for (int i = 0; i < columns; i++){
+            mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
+        }
+    }
+
+    private void fillGridCells(int gridRowCnt, int gridColumnCnt, Vector2d upperLeft) {
+        for (int gridRow = 0; gridRow < gridRowCnt; gridRow++) {
+            for (int gridColumn = 0; gridColumn < gridColumnCnt; gridColumn++) {
+                var mapPosition = upperLeft.add(new Vector2d(gridColumn, -gridRow));
+                Label field = new Label();
+                field.setText(this.worldMap.isOccupied(mapPosition) ? this.worldMap.objectAt(mapPosition).toString() : " ");
+                this.mapGrid.add(field,  gridColumn, gridRow, 1, 1);
+
+                // Set horizontal and vertical alignment to center
+                GridPane.setHalignment(field, HPos.CENTER);
+                GridPane.setValignment(field, VPos.CENTER);
+            }
+        }
+    }
+
+    private void clearGrid() {
+        mapGrid.getChildren().retainAll(mapGrid.getChildren().getFirst()); // hack to retain visible grid lines
+        mapGrid.getColumnConstraints().clear();
+        mapGrid.getRowConstraints().clear();
+    }
+
     public void drawMap(String message) {
         moveInfoLabel.setText(message);
-        infoLabel.setText(worldMap.toString());
+        this.clearGrid();
+        this.buildMapGrid();
     }
 
     @Override
