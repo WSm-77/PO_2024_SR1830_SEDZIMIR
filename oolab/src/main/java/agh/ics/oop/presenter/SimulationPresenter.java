3de77e4e97689f13f4277cc8c4bb8561f76f27a2
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-12-13 13:06:19 +0100

    Move simulation start logic to SimulationPresenter.onSimulationStartClicked()

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -1,13 +1,23 @@
 package agh.ics.oop.presenter;
 
-import agh.ics.oop.model.MapChangeListener;
-import agh.ics.oop.model.WorldMap;
+import agh.ics.oop.OptionsParser;
+import agh.ics.oop.Simulation;
+import agh.ics.oop.model.*;
+import agh.ics.oop.model.util.ConsoleMapDisplay;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.List;
+
 public class SimulationPresenter implements MapChangeListener {
+    public static final String NO_MOVES_PROVIDED_ALERT_TITLE = "No moves provided";
+    public static final String NO_MOVES_PROVIDED_ALERT_MESSAGE = "Please enter animal moves";
+    public static final String ILLEGAL_ARGUMENT_ALERT_TITLE = "Illegal argument alert";
+
     @FXML
     private TextField movesTextEntry;
 
@@ -22,13 +32,6 @@ public class SimulationPresenter implements MapChangeListener {
 
     private WorldMap worldMap;
 
-    @FXML
-    public void initialize() {
-        startButton.setOnAction(actionEvent -> {
-            // TODO
-        });
-    }
-
     public void setWorldMap(WorldMap map) {
         this.worldMap = map;
     }
@@ -40,7 +43,55 @@ public class SimulationPresenter implements MapChangeListener {
 
     @Override
     public void mapChanged(WorldMap worldMap, String message) {
-        System.out.println("MapChange!!!!");
         this.drawMap(message);
+    }
+
+    public void onSimulationStartClicked(ActionEvent actionEvent) {
+        List<MoveDirection> directions = null;
+
+        String movesString = this.movesTextEntry.getText();
+
+        if (movesString.isEmpty()) {
+            this.showNoMovesProvidedAlert();
+            return;
+        }
+
+        try {
+            directions = OptionsParser.parseStringArray(movesString.split(" "));
+        } catch (IllegalArgumentException exception) {
+            this.showIllegalArgumentsAlert(exception.getMessage());
+            return;
+        }
+
+        // Simulation
+        var repeatedPosition = new Vector2d(2, 2);
+        List<Vector2d> positions = List.of(repeatedPosition, repeatedPosition, new Vector2d(3, 4));
+
+        MapChangeListener consoleLog = new ConsoleMapDisplay();
+
+        // 1. RectangularMap Simulation
+        var rectangularMap = new RectangularMap(4, 4);
+
+        rectangularMap.subscribe(consoleLog);
+        rectangularMap.subscribe(this);
+        this.setWorldMap(rectangularMap);
+
+        var rectangularMapSimulation = new Simulation(positions, directions, rectangularMap);
+
+        rectangularMapSimulation.run();
+    }
+
+    private void showNoMovesProvidedAlert() {
+        Alert illegalArgumentAlert = new Alert(Alert.AlertType.WARNING);
+        illegalArgumentAlert.setTitle(SimulationPresenter.NO_MOVES_PROVIDED_ALERT_TITLE);
+        illegalArgumentAlert.setHeaderText(SimulationPresenter.NO_MOVES_PROVIDED_ALERT_MESSAGE);
+        illegalArgumentAlert.show();
+    }
+
+    private void showIllegalArgumentsAlert(String exceptionMessage) {
+        Alert illegalArgumentAlert = new Alert(Alert.AlertType.ERROR);
+        illegalArgumentAlert.setTitle(SimulationPresenter.ILLEGAL_ARGUMENT_ALERT_TITLE);
+        illegalArgumentAlert.setHeaderText(exceptionMessage);
+        illegalArgumentAlert.show();
     }
 }
