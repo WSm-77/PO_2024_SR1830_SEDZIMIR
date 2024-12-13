2b591240f025ebe735d95b22c9659618d52e38ee
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2024-12-13 12:03:51 +0100

    Add start Button and TextFiled for entering animals moves

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -3,13 +3,28 @@ package agh.ics.oop.presenter;
 import agh.ics.oop.model.MapChangeListener;
 import agh.ics.oop.model.WorldMap;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SimulationPresenter implements MapChangeListener {
+    @FXML
+    private TextField movesTextEntry;
+
+    @FXML
+    private Button startButton;
+
     @FXML
     private Label infoLabel;
 
     private WorldMap worldMap;
+
+    @FXML
+    void initialize() {
+        startButton.setOnAction(actionEvent -> {
+            // TODO
+        });
+    }
 
     public void setWorldMap(WorldMap map) {
         this.worldMap = map;
